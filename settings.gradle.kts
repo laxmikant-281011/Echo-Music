ba79656cc8f7b037f5a91371b2e649ffeb1becd0
laxmikant-281011 <laxmikant2810aksc0@gmail.com>
2026-09-23 21:30:27 +0530

    Rename root project from 'Mine Music' to 'Anshika'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
 //     id("org.gradle.toolchains.foojay-resolver-convention") version("1.0.0")
 // }
 
-rootProject.name = "Mine Music"
+rootProject.name = "Anshika"
 include(
     ":app",
     ":innertube",
