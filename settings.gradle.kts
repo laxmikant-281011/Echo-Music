8c12e668fde8ab1d0554eb54ae2cafe373b7c650
laxmikant-281011 <laxmikant2810aksc0@gmail.com>
2026-09-24 17:03:46 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
 //     id("org.gradle.toolchains.foojay-resolver-convention") version("1.0.0")
 // }
 
-rootProject.name = "Anshika"
+rootProject.name = "Mine-Music"
 include(
     ":app",
     ":innertube",
